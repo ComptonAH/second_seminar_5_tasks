c0e70a8493f4e2ed1a820e555f94e70ed867d21e
Nikita Karetkin <nikita-kareta@yandex.ru>
2023-02-01 11:50:39 +0300

    First excercise is completed

---
+++ exercise1/Program.cs
@@ -0,0 +1,22 @@
+﻿int RandomNumber = new Random().Next(10,99);
+Console.WriteLine(RandomNumber);
+
+// First solution:
+// int Remainder = RandomNumber%10;
+// int Whole = RandomNumber/10;
+// if (Whole > Remainder)
+// {
+//     Console.WriteLine($"The bigger digit is {Whole}");
+// }
+// else
+// {
+//     Console.WriteLine($"The bigger digit is {Remainder}");
+// }
+
+// Second solution:
+// int Remainder = RandomNumber%10;
+// int Whole = RandomNumber/10;
+
+//Third solution (the most optimal):
+int Max = Math.Max(RandomNumber%10,RandomNumber/10);
+Console.WriteLine($"The bigger digit is {Max}");
