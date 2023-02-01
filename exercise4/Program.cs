f5eeea2f261996e8751eafd97956baeee38895be
Nikita Karetkin <nikita-kareta@yandex.ru>
2023-02-01 12:46:52 +0300

    Exercises had been supplemented

--- exercise4/Program.cs
+++ exercise4/Program.cs
@@ -7,18 +7,29 @@ Console.Write("Enter the second integer number: ");
 int Num2 = Convert.ToInt32(Console.ReadLine());
 Num2 = Math.Abs(Num2);
 
-if (Num1 == Math.Sqrt(Num2))
+//First solution (is more detailed):
+// if (Num1 == Math.Sqrt(Num2))
+// {
+//     Console.WriteLine($"{Num1} is square root of {Num2}");
+// }
+// else
+// {
+//     if (Num2 == Math.Sqrt(Num1))
+//     {
+//         Console.WriteLine($"{Num2} is square root of {Num1}");
+//     }
+//     else
+//     {
+//         Console.WriteLine("None of your numbers is a square root");
+//     }
+// }
+
+//Second solution:
+if (Num1 == Math.Pow(Num2,2) || Num2 == Math.Pow(Num1,2))
 {
-    Console.WriteLine($"{Num1} is square root of {Num2}");
+    Console.WriteLine("One of your number is a square root of the second one");
 }
 else
 {
-    if (Num2 == Math.Sqrt(Num1))
-    {
-        Console.WriteLine($"{Num2} is square root of {Num1}");
-    }
-    else
-    {
-        Console.WriteLine("None of your numbers is a square root");
-    }
+    Console.WriteLine("None of your numbers is a square root of the second one");
 }
--- exercise5/Program.cs
+++ exercise5/Program.cs
@@ -1,19 +1,30 @@
 ﻿Console.Write("Enter an integer number: ");
 int Number = Convert.ToInt32(Console.ReadLine());
 
-if (Number%7==0)
-{
-    if (Number%23==0)
-    {
-       Console.WriteLine($"{Number} is a simultaneously divisible number of 7 and 23"); 
-    }
-    else
-    {
-        Console.WriteLine($"{Number} is not a divisible number of 23, but of 7"); 
-    }
+// First solution (is more detailed):
+// if (Number%7==0)
+// {
+//     if (Number%23==0)
+//     {
+//        Console.WriteLine($"{Number} is a simultaneously divisible number of 7 and 23"); 
+//     }
+//     else
+//     {
+//         Console.WriteLine($"{Number} is not a divisible number of 23, but of 7"); 
+//     }
     
+// }
+// else
+// {
+//     Console.WriteLine($"{Number} is not a divisible number of 7 and 23");
+// }
+
+//Second solution:
+if (Number%7==0 && Number%23==0)
+{
+    Console.WriteLine($"{Number} is a simultaneously divisible number of 7 and 23"); 
 }
 else
 {
-    Console.WriteLine($"{Number} is not a divisible number of 7 and 23");
+    Console.WriteLine($"{Number} is NOT a simultaneously divisible number of 7 and 23"); 
 }
