8f2edb8129ac65c23d7710b0f6764ef88314f059
Nikita Karetkin <nikita-kareta@yandex.ru>
2023-02-01 12:06:36 +0300

    Exercise 3 is completed

---
+++ exercise3/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Enter the first integer number: ");
+int Num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter the second integer number: ");
+int Num2 = Convert.ToInt32(Console.ReadLine());
+
+if (Num1%Num2==0)
+{
+    Console.WriteLine($"{Num2} is a divisible number to {Num1}");
+}
+else
+{
+    int Remainder = Num1%Num2;
+    Console.WriteLine($"The remainder is {Remainder}");
+}
